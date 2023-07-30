9527ece466b4070bca3a46f9e059b31c7e42717e
SergeiMironenko <90122255+SergeiMironenko@users.noreply.github.com>
2023-07-30 16:47:12 +0700

    add 5.1

---
+++ 5/test/server.c
@@ -0,0 +1,45 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include "erproc.h"
+
+int main(int argc, char const* argv[])
+{
+    int server = Socket(AF_INET, SOCK_DGRAM, 0);
+
+    struct sockaddr_in adr = {0};
+    adr.sin_family = AF_INET;
+    adr.sin_port = htons(34543);
+    Bind(server, (struct sockaddr*) &adr, sizeof adr);
+
+    Listen(server, 5);
+
+    socklen_t adrlen = sizeof adr;
+    int fd = Accept(server, (struct sockaddr*) &adr, &adrlen);
+
+    ssize_t nread;
+    char buf[256];
+    nread = read(fd, buf, 256);
+    if (nread == -1)
+    {
+        perror("read failed");
+        exit(EXIT_FAILURE);
+    }
+    if (nread == 0)
+    {
+        printf("end of file\n");
+    }
+    write(STDOUT_FILENO, buf, nread);
+    write(fd, buf, nread);
+
+    sleep(1);
+
+    close(fd);
+    close(server);
+
+    return 0;
+}
